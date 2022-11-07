4feba25f9ee913481c459271a954b2b7911d04b9
trickaugusto <trickaugusto1206@gmail.com>
2022-11-07 15:12:57 -0300

    aula 2

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'johN@gmail.com',
+            avataUrl: 'https://github.com/trickaugusto.png'
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: 'example pool',
+            code: 'BOL123',
+            ownerId: user.id,
+
+            participant: {
+                create: {
+                    userId: user.id
+                }
+            }
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-04T20:19:38.964Z',
+            firstTeamCountryCode: 'DE',
+            secondTeamCountryCode: 'BR'
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-03T20:19:38.964Z',
+            firstTeamCountryCode: 'BR',
+            secondTeamCountryCode: 'AR',
+
+            guesses: {
+                create: {
+                    firstTeamPoints: 2,
+                    secondTeamPoints: 1,
+
+                    participant: {
+                        connect: {
+                            userId_poolId: {
+                                userId: user.id,
+                                poolId: pool.id
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+main()
